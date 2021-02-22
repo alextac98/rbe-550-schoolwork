7cc7aee054a199c9de9daffd3263a502c682752f
Alex Tacescu <alextac98@gmail.com>
2021-02-22 00:34:19 -0500

    Add manhattan distance + euclidean distance calculator functions in map2d

--- hw_1/map2d.py
+++ hw_1/map2d.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 class map2d:
     def __init__(self, grid: list):
         self.map = grid
@@ -26,3 +28,9 @@ class map2d:
             return neighbor
         else:
             return None
+    
+    def get_manhattan_distance(self, point1: list, point2: list):
+        return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+    def get_euclidean_distance(self, point1: list, point2: list):
+        return math.sqrt(sum([(p1 - p2) ** 2 for p1, p2 in zip(point1, point2)]))
